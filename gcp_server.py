896934a53311929f730e09143d4505385d57d5d4
AryusG <verdyg24@gmail.com>
2022-07-15 23:37:35 +1000

    added gcp_server_function

--- gcp_server.py
+++ gcp_server.py
@@ -1,29 +1,43 @@
-import socket 
 
-def send_to_gcp_function(destination_email: str):
-  gcp_fn_url = f"https://australia-southeast1-cybernetic-muse-356101.cloudfunctions.net/send-email?destination_email={destination_email}"
-  
+import smtplib
+from email.message import EmailMessage
+import ssl
+from flask import escape
+import functions_framework
 
-s = socket.socket()
-print("Socket Successfully Created")
-port = 3389
-s.bind(('', port))
-print(f"Socket Binded to Port {port}")
-s.listen(5)
-print("Socket is Listening...")
+@functions_framework.http
+def send_email(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
 
+    if request_json and 'destination_email' in request_json:
+        destination_email = request_json['destination_email']
+    elif request_args and 'destination_email' in request_args:
+        destination_email = request_args['destination_email']
+    else:
+        destination_email = 'World'
 
-while True:
-  c_socket, address = s.accept()
-  print(f"Got Connection From {address}")
+    my_email = "verdysworld@gmail.com"
+    my_email_pass = "mascwseuajhxjenh"
+    
+    subject = "Test Email"
+    body = '''This is a test email'''
+    em = EmailMessage()
+    em['From'] = my_email
+    em['To'] = destination_email
+    em['Subject'] = subject
+    em.set_content(body)
 
-  data = c_socket.recv(1024)
-  if len(data) > 0:
-    destination_email = data.decode()
-    send_to_gcp_function(destination_email=destination_email) 
-    c_socket.send("Email has been sent.".encode())
+    ssl_context = ssl.create_default_context()
 
-  c_socket.send("Thank you for connecting!".encode())
-  c_socket.close()
+    with smtplib.SMTP_SSL('smtp.gmail.com', context=ssl_context) as smtp:
+        smtp.login(my_email, my_email_pass)
+        try:
+            smtp.sendmail(my_email, destination_email, em.as_string())
+            print(f"Email has been sent from {my_email} to {destination_email}")
+        except smtplib.SMTPResponseException as e:
+            print(f"Error with: {e}")
+    
+    return 'Email sent to {}!'.format(escape(destination_email))
   
   
